b5de7c0d8f6fdaaaa81721b08c1da30a05a07180
Your-name <Your-email>
2019-12-11 19:27:16 +0200

    get rajapinta lisatty

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 console.log("ooot tyhme jos naat tan");
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log("syokanaa"));
+
+port = process.env.PORT || 3000;
+ app.listen(port, () => console.log("syokanaa"));
+
 app.use(express.static("public"));
 console.log("kokeilumuutos");
+
+app.use(express.json({limit: '1mb'}));
+
+const hyvaa =[
+
+{
+  "ajantuhlaajat": "pelin tekijä",
+  "maissi": "ainakin 1"
+
+},
+{
+"botti": "kirjain",
+"pisteet": "enemmän kun sulla"
+}
+]
+
+app.get('/api/pisteet', function (request, response) {
+ response.send(hyvaa);
+})
